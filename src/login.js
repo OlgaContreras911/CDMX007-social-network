56070b906506ba48ac011a9c31f6646ecb571d9d
OlgaContreras911 <rockersita.911@gmail.com>
2019-03-21 12:28:28 -0600

    agregando cambio en log in

--- src/login.js
+++ src/login.js
@@ -16,15 +16,15 @@ var provider = new firebase.auth.GoogleAuthProvider();
   //var credential = error.credential;
   // ...
 //});
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
+//firebase.auth().getRedirectResult().then(function(result) {
+  //if (result.credential) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     //var token = result.credential.accessToken;
     // ...
-  }
+  //}
   // The signed-in user info.
   //var user = result.user;
-}).catch(function(error) {
+//}).catch(function(error) {
   // Handle Errors here.
   //var errorCode = error.code;
   //var errorMessage = error.message;
@@ -33,12 +33,12 @@ firebase.auth().getRedirectResult().then(function(result) {
   // The firebase.auth.AuthCredential type that was used.
   //var credential = error.credential;
   // ...
-});
+//});
 
 var ui = new firebaseui.auth.AuthUI(firebase.auth());   
 var uiConfig = {
     callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      signInSuccessWithAuthResult: function() {
         // User successfully signed in.
         // Return type determines whether we continue the redirect automatically
         // or whether we leave that to developer to handle.
